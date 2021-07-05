8fca352cb5f754cf88ed0ef30952a207d4d970d4
Emily Bowe <emily.bowe@r3.com>
2021-07-05 23:00:32 +0100

    added tests for checkEdge method

--- src/test/kotlin/GameTest.kt
+++ src/test/kotlin/GameTest.kt
@@ -112,16 +112,93 @@ internal class GameTest {
         assertFalse(game.left())
     }
 
+    //stack order:  1)left 2)right 3)down 4)up
+
+    @Test
+    fun checkEdgeYEdge() {
+        val paths = ArrayList<PathGen>()
+        matrixDefault = 1
+        val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
+        game.locationX = 1
+        game.locationY = mazeSize - 1
+        game.checkEdge()
+        assertTrue(game.dfs.size == 3)
+        assertEquals(Pair(0, mazeSize - 1), game.dfs.first()) //left
+        assertEquals(Pair(2, mazeSize - 1), game.dfs[1]) //right #down elim
+        assertEquals(Pair(1, mazeSize - 2), game.dfs.last()) //up
+        println(game.dfs.first())
+        println(game.dfs[1])
+        println(game.dfs.last())
+    }
+
+    @Test
+    fun checkEdgeXEdge() {
+        val paths = ArrayList<PathGen>()
+        matrixDefault = 1
+        val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
+        game.locationX = mazeSize - 1
+        game.locationY = 1
+        game.checkEdge()
+        assertTrue(game.dfs.size == 3)
+        assertEquals(Pair(mazeSize - 2, 1), game.dfs.first()) //left #right elim
+        assertEquals(Pair(mazeSize - 1, 2), game.dfs[1]) //down
+        assertEquals(Pair(mazeSize - 1, 0), game.dfs.last()) //up
+        println(game.dfs.first())
+        println(game.dfs[1])
+        println(game.dfs.last())
+    }
+
+    @Test
+    fun checkEdgeY0() {
+        val paths = ArrayList<PathGen>()
+        matrixDefault = 1
+        val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
+        game.locationX = 1
+        game.locationY = 0
+        game.checkEdge()
+        assertTrue(game.dfs.size == 3)
+        assertEquals(Pair(0, 0), game.dfs.first()) //left
+        assertEquals(Pair(2, 0), game.dfs[1]) //right
+        assertEquals(Pair(1, 1), game.dfs.last()) //down #up elim
+        println(game.dfs.first())
+        println(game.dfs[1])
+        println(game.dfs.last())
+    }
+
+    @Test
+    fun checkEdgeX0() {
+        val paths = ArrayList<PathGen>()
+        matrixDefault = 1
+        val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
+        game.locationX = 0
+        game.locationY = 1
+        game.checkEdge()
+        assertTrue(game.dfs.size == 3)
+        assertEquals(Pair(1, 1), game.dfs.first()) //#left elim, right
+        assertEquals(Pair(0, 2), game.dfs[1]) //down
+        assertEquals(Pair(0, 0), game.dfs.last()) //up
+        println(game.dfs.first())
+        println(game.dfs[1])
+        println(game.dfs.last())
+    }
+
     @Test
     fun checkEdge() {
         val paths = ArrayList<PathGen>()
         matrixDefault = 1
         val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
-        val dfs = ArrayDeque<Pair<Int, Int>>()
         game.locationX = 1
         game.locationY = 1
-
-
+        game.checkEdge()
+        assertTrue(game.dfs.size == 4)
+        assertEquals(Pair(0, 1), game.dfs.first()) //left
+        assertEquals(Pair(2, 1), game.dfs[1]) //right
+        assertEquals(Pair(1, 2), game.dfs[2]) //down
+        assertEquals(Pair(1, 0), game.dfs.last()) //up
+        println(game.dfs.first())
+        println(game.dfs[1])
+        println(game.dfs[2])
+        println(game.dfs.last())
     }
 
     @Test
@@ -129,7 +206,6 @@ internal class GameTest {
         val paths = ArrayList<PathGen>()
         matrixDefault = 1
         val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
-        val dfs = ArrayDeque<Pair<Int, Int>>()
         game.locationX = 1
         game.locationY = 1
         game.dfs.addFirst(Pair(1,0))
@@ -142,7 +218,6 @@ internal class GameTest {
         val paths = ArrayList<PathGen>()
         matrixDefault = 1
         val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
-        val dfs = ArrayDeque<Pair<Int, Int>>()
         game.locationX = 1
         game.locationY = 1
         game.addUp()
@@ -154,7 +229,6 @@ internal class GameTest {
         val paths = ArrayList<PathGen>()
         matrixDefault = 1
         val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
-        val dfs = ArrayDeque<Pair<Int, Int>>()
         game.locationX = 1
         game.locationY = 1
         game.dfs.addFirst(Pair(1,2))
@@ -167,7 +241,6 @@ internal class GameTest {
         val paths = ArrayList<PathGen>()
         matrixDefault = 1
         val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
-        val dfs = ArrayDeque<Pair<Int, Int>>()
         game.locationX = 1
         game.locationY = 1
         game.addDown()
@@ -179,7 +252,6 @@ internal class GameTest {
         val paths = ArrayList<PathGen>()
         matrixDefault = 1
         val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
-        val dfs = ArrayDeque<Pair<Int, Int>>()
         game.locationX = 1
         game.locationY = 1
         game.dfs.addFirst(Pair(2,1))
@@ -192,7 +264,6 @@ internal class GameTest {
         val paths = ArrayList<PathGen>()
         matrixDefault = 1
         val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
-        val dfs = ArrayDeque<Pair<Int, Int>>()
         game.locationX = 1
         game.locationY = 1
         game.addRight()
@@ -204,7 +275,6 @@ internal class GameTest {
         val paths = ArrayList<PathGen>()
         matrixDefault = 1
         val game = Game(mazeSize, matrixDefault, entryX, entryY, goalX, goalY, paths)
-        val dfs = ArrayDeque<Pair<Int, Int>>()
         game.locationX = 1
         game.locationY = 1
         game.dfs.addFirst(Pair(0,1))
