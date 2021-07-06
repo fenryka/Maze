3439810f3070cbf2f791f7ca4012d5ae7af6c029
Katelyn Baker <katelyn.faith.baker@gmail.com>
2021-07-06 22:40:15 +0100

    Can't run jar as executable, fix build.gradle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,3 +17,12 @@ fun main() {
     println(game.maze)
 }
 
+class Main {
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            main()
+        }
+    }
+}
