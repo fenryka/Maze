bf87865233ae71ca2bedf68c610f2d78e1807c13
Katelyn Baker <katelyn.faith.baker@gmail.com>
2021-07-06 22:24:02 +0100

    Make Renderer pluggable

--- src/main/kotlin/Maze.kt
+++ src/main/kotlin/Maze.kt
@@ -1,6 +1,34 @@
 import java.lang.StringBuilder
-class Maze (val matrixValue: Int, matrixDefault: Int, entryX: Int, entryY: Int, goalX: Int, goalY: Int, paths: List<PathGen>) {
+
+class Maze (
+    val matrixValue: Int,
+    matrixDefault: Int,
+    entryX: Int, entryY:
+    Int, goalX: Int,
+    goalY: Int,
+    paths: List<PathGen>,
+    private val renderer: Renderer = defaultRenderer()
+) {
     var matrix = Array(matrixValue) {IntArray(matrixValue){matrixDefault} }
+
+    interface Renderer {
+        fun render(maze: Maze) : String
+    }
+
+    class defaultRenderer : Renderer {
+        override fun render(maze: Maze) : String {
+            return StringBuilder().apply {
+                for (i in 0 until maze.matrixValue) {
+                    for (j in 0 until maze.matrixValue) {
+                        append(maze.matrix[j][i].toString() + if (j<maze.matrixValue - 1)  " " else "")
+                    }
+                    if (i < maze.matrixValue - 1) {
+                        append("\n")
+                    }
+                }
+            }.toString()
+        }
+    }
 
     init {
         for(i in paths) {
@@ -18,15 +46,6 @@ class Maze (val matrixValue: Int, matrixDefault: Int, entryX: Int, entryY: Int, 
         matrix[entryX][entryY] = 2
     }
 
-    override fun toString() = StringBuilder().apply {
-        for (i in 0 until matrixValue) {
-            for (j in 0 until matrixValue) {
-                append(matrix[j][i].toString() + if (j<matrixValue - 1)  " " else "")
-            }
-            if (i < matrixValue - 1) {
-                append("\n")
-            }
-        }
-    }.toString()
+    override fun toString() = renderer.render(this)
 }
 
